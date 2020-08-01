487c4f269b77d95929707cf91a4687d3eb664db4
Kenzo Ymagawa <alvaroymagawa@gmail.com>
2020-07-31 21:51:36 -0300

    create transaction POST route

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
 
+// CUSTOM IMPORTS
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+    const categoryRepository = getRepository(Category);
+
+    if (type !== 'income' && type !== 'outcome')
+      throw new AppError('Field type is not valid.', 401);
+
+    const existingCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    let newCategory;
+
+    if (!existingCategory) {
+      newCategory = await categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(newCategory);
+    }
+    const transaction = await transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: existingCategory ? existingCategory.id : newCategory?.id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
